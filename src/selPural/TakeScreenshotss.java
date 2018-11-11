f48b65d3bfd99585be0970de6a8f2750b66fcb2d
UdaniSoo <isuru.udani1@gmail.com>
2018-11-11 12:44:37 +0800

    Take screenshot using selenium

---
+++ src/selPural/TakeScreenshotss.java
@@ -0,0 +1,26 @@
+package selPural;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
+
+public class TakeScreenshotss {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp");
+		
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		// Now you can do whatever you need to do with it, for example copy somewhere
+		FileUtils.copyFile(scrFile, new File("C:\\Users\\Udani\\eclipse-workspace\\SeleniumPlu\\Google.png"));
+
+	}
+
+}
